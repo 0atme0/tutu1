1b488b314cf48e3f24de8608918c4609b33183f1
0atme0 <0atme0@gmail.com>
2016-06-23 13:54:34 +0300

    first commit

---
+++ Pods/Target Support Files/Pods-tutu-test/Pods-tutu-test-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tutu_test : NSObject
+@end
+@implementation PodsDummy_Pods_tutu_test
+@end
---
+++ Pods/Target Support Files/Pods-tutu-test/Pods-tutu-test-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_tutu_testVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_tutu_testVersionString[];
+
